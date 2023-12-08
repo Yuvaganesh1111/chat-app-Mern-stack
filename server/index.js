d1b34c12bcf9aba4e9ddbb1f586e1280642e7398
Yuvaganesh1111 <yuvaganeshyg1111@gmail.com>
2023-12-08 17:31:46 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express=require('express');
+const app=express();
+
+const {Server}=require('socket.io');
+const http=require('http');
+const cors=require('cors');
+app.use(cors());
+const server=http.createServer(app);
+
+const io =new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        method:['get','post']
+    }
+})
+io.on("connection",(socket)=>{
+    console.log(`user connected id ${socket.id}`)
+    socket.on("room",(data)=>{
+        socket.join(data);
+     })
+    socket.on("send_message",(data)=>{
+       socket.to(data.room).emit("recived_message",data)
+    })
+})
+server.listen(3001,()=>{
+    console.log("server is running on 3001")
+})
+
+
+

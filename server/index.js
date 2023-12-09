de63b37a5edd94589d3d018f0ecf90f2785b7879
Yuvaganesh1111 <90375978+Yuvaganesh1111@users.noreply.github.com>
2023-12-09 16:53:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server=http.createServer(app);
 
 const io =new Server(server,{
     cors:{
-        origin:["https://chat-app-yuva-frontend.vercel.app"],
+        origin:["http://localhost:3001"],
         method:['get','post'],
         credentials:true
     }

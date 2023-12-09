ae20b6342357bf0a0ad56b1e42b26af27d3e8606
Yuvaganesh1111 <90375978+Yuvaganesh1111@users.noreply.github.com>
2023-12-09 16:15:14 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,19 +4,17 @@ const app=express();
 const {Server}=require('socket.io');
 const http=require('http');
 const cors=require('cors');
-
+app.use(cors());
 const server=http.createServer(app);
 
 const io =new Server(server,{
     cors:{
         origin:["https://chat-app-yuva-frontend.vercel.app"],
-        method:['POST','GET'],
+        method:['get','post'],
         credentials:true
     }
 })
-app.get('/',(req,res)=>{
-    res.send("hello");
-})
+
 io.on("connection",(socket)=>{
     console.log(`user connected id ${socket.id}`)
     socket.on("room",(data)=>{
